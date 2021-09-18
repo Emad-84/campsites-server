39fb053f986dd96e36fa0ce92f904eff301df9db
Emad Adas <emad.adas@yahoo.com>
2021-09-18 17:06:23 +0300

    OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "925548954984929",
+    clientSecret: "72593c29051d4d1beb8d19282ebb17c5",
+  },
 };
--- modals/user.js
+++ modals/user.js
@@ -11,6 +11,7 @@ const userSchema = new Schema({
     type: String,
     default: "",
   },
+  facbookId: String,
   admin: {
     type: Boolean,
     default: false,
